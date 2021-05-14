9f52668321060f216337ae91a16f26aab4f3a32e
aure-nogueras <anogueras@correo.ugr.es>
2021-05-14 05:12:44 +0200

    Modificada ruta de archivos

--- limpieza_datos.py
+++ limpieza_datos.py
@@ -3,9 +3,9 @@ import pandas as pd
 # Limpieza de los datos
 
 col_list = ["datetime", "San Francisco"]
-reader = pd.read_csv("humidity.csv", usecols=col_list)
+reader = pd.read_csv("/tmp/humidity.csv", usecols=col_list)
 reader = reader.rename(columns = {'San Francisco':'HUM'})
-reader2 = pd.read_csv("temperature.csv", usecols=col_list)
+reader2 = pd.read_csv("/tmp/temperature.csv", usecols=col_list)
 reader2 = reader2.rename(columns = {'San Francisco':'TEMP'})
 
 merged_inner = pd.merge(left=reader2, right=reader, left_on='datetime', right_on='datetime')
